a50ac5bf65beeaad8dcc5bf3775b3a7c43b0ba96
Sandesh <sandeshtiwari0211@gmail.com>
2023-12-21 12:44:19 +0545

    something idk

--- Project/Frontend-Main/main.js
+++ Project/Frontend-Main/main.js
@@ -1,35 +1,65 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('imageForm');
+//document.addEventListener('DOMContentLoaded', function () {
+//    const form = document.getElementById('imageForm');
+//
+//    form.addEventListener('submit', async function (event) {
+//        event.preventDefault();
+//
+//        const fileInput = document.getElementById('imageInput');
+//        const file = fileInput.files[0];
+//
+//        const formData = new FormData();
+//        formData.append('file', file);
+//
+//        try {
+//            const response = await fetch('http://localhost:8000/predict', {
+//                method: 'POST',
+//                body: formData,
+//            });
+//
+//            if (response.ok) {
+//                const result = await response.json();
+//                console.log('Prediction result:', result);
+//
+//                const resultContainer = document.getElementById('resultContainer');
+//                resultContainer.innerHTML = `
+//                    <p>Predicted Class: ${result.class}</p>
+//                    <p>Confidence: ${result.confidence}</p>
+//                `;
+//            } else {
+//                console.error('Error:', response.status, response.statusText);
+//            }
+//        } catch (error) {
+//            console.error('Fetch error:', error);
+//        }
+//    });
+//});
 
-    form.addEventListener('submit', async function (event) {
+document.addEventListener('DOMContentLoaded', function () {
+    const uploadForm = document.querySelector('#uploadForm');
+
+    uploadForm.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        const fileInput = document.getElementById('imageInput');
+        const fileInput = document.querySelector('input[type="file"]');
         const file = fileInput.files[0];
 
         const formData = new FormData();
         formData.append('file', file);
 
-        try {
-            const response = await fetch('http://localhost:8000/predict', {
-                method: 'POST',
-                body: formData,
-            });
-
-            if (response.ok) {
-                const result = await response.json();
-                console.log('Prediction result:', result);
-
-                const resultContainer = document.getElementById('resultContainer');
-                resultContainer.innerHTML = `
-                    <p>Predicted Class: ${result.class}</p>
-                    <p>Confidence: ${result.confidence}</p>
-                `;
-            } else {
-                console.error('Error:', response.status, response.statusText);
-            }
-        } catch (error) {
-            console.error('Fetch error:', error);
-        }
+        // Make a POST request to the FastAPI backend
+        fetch('http://localhost:8000/predict', {
+            method: 'POST',
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the backend
+            console.log(data);
+            alert(`Predicted class: ${data.class}\nConfidence: ${data.confidence}`);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Error predicting image. Please try again.');
+        });
     });
 });
