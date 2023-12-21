b4d3aed873479baf04f2fef1a4c7c6c75d3d3455
Sandesh <sandeshtiwari0211@gmail.com>
2023-12-21 21:30:04 +0545

    UI Improvements

--- Project/Frontend-Main/main.js
+++
@@ -1,8 +0,0 @@
-const form=document.querySelector("form");
-form.addEventListener('submit',(e)=>{
-e.preventDefault();
-const formData=new FormData(form);
-axios.post('http://localhost:8000/predict',formData)
-.then(res=> console.log(res))
-.catch(err => console.log(err))
-});
